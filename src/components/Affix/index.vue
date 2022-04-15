855018aff5ba6cfba2614fc442f46885a9dd4757
laoqitong <laoqitong727683@163.com>
2022-04-15 14:21:19 +0800

    feat✨:  Affix 固钉新增

---
+++ src/components/Affix/index.vue
@@ -0,0 +1,139 @@
+<template>
+  <div
+    class="affix-placeholder"
+    :style="wrapStyle"
+  >
+    <div
+      :class="{'affix': affixed}"
+      :style="styles"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+/**
+* @file Affix.vue
+* @author laoqitong
+* @date 20220415 13:56:58
+*/
+export default {
+  name: 'Affix',
+  props: {
+    // 距离窗口顶部达到指定偏移量后触发
+    offset: {
+      type: Number,
+      default: 0
+    },
+    // 固定状态改变时触发的回调函数
+    onAffix: {
+      type: Function,
+      default() { }
+    },
+    // 设置 Affix 的活动范围，值为affix上级元素的id(可以是父元素，也可以是父元素的父元素...)
+    boundary: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      affixed: false,
+      styles: {},
+      affixedClientHeight: 0,
+      wrapStyle: {}
+    }
+  },
+  computed: {
+    offsets() {
+      if (this.boundary) {
+        return 0
+      }
+      return this.offset
+    }
+  },
+  mounted() {
+    this.affixedClientHeight = this.$el.children[0].clientHeight
+    this.wrapStyle = { height: `${this.affixedClientHeight}px` }
+    window.addEventListener('scroll', this.handleScroll)
+    window.addEventListener('resize', this.handleScroll)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.handleScroll)
+    window.removeEventListener('resize', this.handleScroll)
+  },
+  methods: {
+    getScroll(w, top) {
+      let ret = w[`page${(top ? 'Y' : 'X')}Offset`]
+      const method = `scroll${top ? 'Top' : 'Left'}`
+      if (typeof ret !== 'number') {
+        const d = w.document
+        // ie6,7,8 standard mode
+        ret = d.documentElement[method]
+        if (typeof ret !== 'number') {
+          // quirks mode
+          ret = d.body[method]
+        }
+      }
+      return ret
+    },
+    getOffset(element) {
+      const rect = element.getBoundingClientRect()
+      const body = document.body
+      const clientTop = element.clientTop || body.clientTop || 0
+      const clientLeft = element.clientLeft || body.clientLeft || 0
+      // const clientHeight = element.clientHeight || 0;
+      const scrollTop = this.getScroll(window, true)
+      const scrollLeft = this.getScroll(window)
+      return {
+        top: rect.bottom + scrollTop - clientTop - this.affixedClientHeight,
+        left: rect.left + scrollLeft - clientLeft
+      }
+    },
+    handleScroll() {
+      const scrollTop = this.getScroll(window, true) + this.offsets // handle setting offset
+      const elementOffset = this.getOffset(this.$el)
+      if (!this.affixed && scrollTop > elementOffset.top) {
+        this.affixed = true
+        this.styles = {
+          top: `${this.offsets}px`,
+          left: `${elementOffset.left}px`,
+          width: `${this.$el.offsetWidth}px`,
+          display: this.affixed ? 'block' : 'none'
+        }
+        this.onAffix(this.affixed)
+      }
+      // if setting boundary
+      if (this.boundary && scrollTop > elementOffset.top) {
+        const el = document.getElementById(this.boundary.slice(1))
+        if (el) {
+          const boundaryOffset = this.getOffset(el)
+          if ((scrollTop + this.offsets) > boundaryOffset.top) {
+            const top = scrollTop - boundaryOffset.top
+            this.styles.top = `-${top}px`
+          }
+        }
+      }
+      if (this.affixed && scrollTop < elementOffset.top) {
+        this.affixed = false
+        this.styles = {}
+        this.onAffix(this.affixed)
+      }
+      if (this.affixed && this.boundary) {
+        const el = document.getElementById(this.boundary.slice(1))
+        if (el) {
+          const boundaryOffset = this.getOffset(el)
+          if ((scrollTop + this.offsets) <= boundaryOffset.top) {
+            this.styles.top = 0
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.affix {
+  position: fixed;
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,14 @@ import ApeTable from './Ape/ApeTable'
 import ApeDrawer from './Ape/ApeDrawer'
 import TreeTable from './TreeTable/index'
 import ModalDialog from './Ape/ModalDialog'
+import Affix from './Affix'
 const install = (Vue) => {
   Vue.component(Pagination.name, Pagination)
   Vue.component(ApeTable.name, ApeTable)
   Vue.component(ApeDrawer.name, ApeDrawer)
   Vue.component(TreeTable.name, TreeTable)
   Vue.component(ModalDialog.name, ModalDialog)
+  Vue.component(Affix.name, Affix)
 }
 
 export default install
