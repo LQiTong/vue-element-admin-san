230950d93ab4926eb825293d1d88f4601e9fda27
laoqitong <laoqitong727683@163.com>
2022-04-18 14:56:54 +0800

    feat✨:  provide  reload 页面刷新方法引入

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view v-if="isRouterAlive" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      isRouterAlive: true
+    }
+  },
+  provide() {
+    return {
+      reload: this.reload
+    }
+  },
+  methods: {
+    reload() {
+      // 强制刷新页面方法
+      this.isRouterAlive = false
+      this.$nextTick(() => {
+        this.isRouterAlive = true
+      })
+    }
+  }
 }
 </script>
