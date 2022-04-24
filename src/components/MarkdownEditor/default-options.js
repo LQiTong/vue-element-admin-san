811bdaa303289f0aa706009ec51514de19390b30
laoqitong <laoqitong727683@163.com>
2022-04-24 18:43:44 +0800

    fix🐛:  @toast-ui/editor 的配置修改补充说明

--- src/components/MarkdownEditor/default-options.js
+++ src/components/MarkdownEditor/default-options.js
@@ -6,7 +6,7 @@ export default {
   useDefaultHTMLSanitizer: true,
   usageStatistics: false,
   hideModeSwitch: false,
-  // !tui-editor 升级成 @toast-ui/editor 修改配置
+  // !tui-editor 升级成 @toast-ui/editor 修改配置 详询 https://baijiahao.baidu.com/s?id=1730947052863348274&wfr=spider&for=pc
   // toolbarItems: ['heading', 'bold', 'italic', 'strike', 'divider', 'hr', 'quote', 'divider', 'ul', 'ol', 'task', 'indent', 'outdent', 'divider', 'table', 'image', 'link', 'divider', 'code', 'codeblock']
   toolbarItems: [['heading', 'bold', 'italic', 'strike'], ['hr', 'quote'], ['ul', 'ol', 'task', 'indent', 'outdent'], ['table', 'image', 'link'], ['code', 'codeblock']]
 }
